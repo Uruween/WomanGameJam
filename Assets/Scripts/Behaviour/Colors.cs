f7121a4e1236538783f3e1fea0ae3387fe4c9f74
Tabibito1148 <camilocarrascal2006@gmail.com>
2025-08-10 02:35:05 -0500

    SaqudeloqtieneArena

--- Assets/Scripts/Behaviour/Colors.cs
+++ Assets/Scripts/Behaviour/Colors.cs
@@ -4,19 +4,32 @@ using System.Collections.Generic;
 public class Colors : MonoBehaviour
 {
     [SerializeField] float distance;
+    [SerializeField] GameObject image;
 
 
     void Update()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distance))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, distance))
         {
-            if(hit.collider.tag == "Door")
+            if (hit.collider.tag == "Door")
             {
-                if(Input.GetKeyDown(KeyCode.F))
+                if (Input.GetKeyDown(KeyCode.F))
                 {
                     hit.collider.transform.GetComponent<Door>().ChangeState();
                 }
+
+                if (hit.distance <= 2)
+                {
+                    image.SetActive(true);
+                }
+            }
+        }
+        else
+        {
+            if (image.activeSelf)
+            {
+                image.SetActive(false);
             }
         }
     }
